7a69d2539562672a337109b7df984a38d30a7f83
João Vitor Muniz Vasconcelos <140927075+JoaoVitorMuniz16@users.noreply.github.com>
2023-10-18 11:15:36 -0300

    Add files via upload

---
+++ JoaoVitor15.py
@@ -0,0 +1,8 @@
+r1 = int(input("insira o comprimento da reta um"))
+r2 = int(input("insira o comprimento da reta dois"))
+r3 = int(input("insira o comprimento da reta um"))
+RF = (r2 + r3)
+if r1 > RF:
+    print ("pode ser formado um triangulo")
+else:
+    print ("pode ser formado um triangulo")
